76fb606d89136737ea77c2d1e94e39221618c96f
James Swank <jmswank7@gmail.com>
2019-04-23 12:52:02 -0600

    stuff

--- members/models.py
+++ members/models.py
@@ -3,17 +3,17 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
-class Members(models.Model):
-    last_name = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    first_name = models.TextField()
-    email = models.EmailField()
-    phone1 = models.CharField(max_length=12)
-    phone2 = models.CharField(max_length=12)
-    address1 = models.TextField()
-    address2 = models.TextField()
-    city = models.TextField()
-    zipcode = models.CharField(max_length=5)
-    notes = models.TextField()
+class Member(models.Model):
+    last_name = models.CharField(max_length=25)
+    first_name = models.CharField(max_length=25)
+    email = models.EmailField(blank=True)
+    phone1 = models.CharField(max_length=12,blank=True)
+    phone2 = models.CharField(max_length=12, blank=True)
+    address1 = models.TextField(blank=True)
+    address2 = models.TextField(blank=True)
+    city = models.CharField(max_length=25,blank=True)
+    zipcode = models.CharField(max_length=5,blank=True)
+    notes = models.TextField(blank=True)
     core_member = models.BooleanField()
     outter_core_member = models.BooleanField()
     events_only_member = models.BooleanField()
